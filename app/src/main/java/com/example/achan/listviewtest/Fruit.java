3a22e9aa3ede21924883303009e86914596d2d59
AchanNono <AchanNono@hotmail.com>
2016-10-12 12:50:49 +0800

    custom listView

---
+++ app/src/main/java/com/example/achan/listviewtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.achan.listviewtest;
+
+/**
+ * Created by Achan on 2016/10/11.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name,int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
